a99aab8ad9aa7651555aa50c438d09cc808293ca
isuru <isurujayawickramat@gmail.com>
2020-11-04 17:02:42 +0530

    Modified 2014_10_12_000000_create_users_table.php and Migrated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,20 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
+            $table->longText('avatar');
+            $table->string('phone')->unique();
             $table->string('password');
+            $table->string('website');
+            $table->string('address');
+            $table->string('city');
+            $table->string('state');
+            $table->string('zip');
+            $table->string('country');
+            $table->text('notes');
             $table->rememberToken();
             $table->timestamps();
         });
